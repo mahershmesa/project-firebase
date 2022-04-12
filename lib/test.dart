4385a775687d6258e4e4f20ef422c0a27cbdecd4
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-12 04:09:39 -0700

    first commit

--- lib/test.dart
+++ lib/test.dart
@@ -25,9 +25,20 @@ class _TestState extends State<Test> {
         children: [
           // ignore: deprecated_member_use
           Center(child: RaisedButton(onPressed: ()async{
-          userCredential =
-      await FirebaseAuth.instance.signInAnonymously();
-      print(userCredential);
+          try {
+    userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    email: "maher@gmaill.com",
+    password: "mahershmesa",
+  );
+} on FirebaseAuthException catch (e) {
+  if (e.code == 'weak-password') {
+    print('The password provided is too weak.');
+  } else if (e.code == 'email-already-in-use') {
+    print('The account already exists for that email.');
+  }
+} catch (e) {
+  print(e);
+}
           },
           child: const Text("Sign In"),))
         ],
