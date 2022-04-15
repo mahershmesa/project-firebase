88c003285dce3e6ecdb959573dc4dba86fbf65bd
DESKTOP-2V0GL2U\FPCC <mahershmesa@gmail.com>
2022-04-15 15:20:18 -0700

    first commit

---
+++ lib/mas.dart
@@ -0,0 +1,15 @@
+class mas extends StatefulWidget {
+  const mas({ Key? key }) : super(key: key);
+
+  @override
+  State<mas> createState() => _masState();
+}
+
+class _masState extends State<mas> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
